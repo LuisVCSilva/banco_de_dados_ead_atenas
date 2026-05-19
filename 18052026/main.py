8d9664b710fa95346a58a5b8dfd4231660b23262
luis <luisviniciuscostasilva@gmail.com>
2026-05-18 21:47:23 -0300

    adicionar classe filme ao main.py e mapear ao bd

--- 18052026/main.py
+++ 18052026/main.py
@@ -11,6 +11,37 @@ filmes = db.table("filmes")
 usuarios = db.table("usuarios")
 User = Query()
 
+#Exemplo de ORM -- Object Relation Mapping
+class Usuario:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.ativo = True
+
+    def to_dict(self):
+        return {"_id":str(uuid4()),"nome":self.nome,"idade":self.idade,"ativo":self.ativo} 
+
+
+@app.post("/usuario/classe")
+def criar_usuario_classe ():
+   dados = request.json
+   if not dados:
+      return {"erro":"cade o pacote?"},400
+   if "nome" not in dados or "idade" not in dados:
+      return {"erro":"faltou nome ou idade no pacote..."}, 400
+   usuario = Usuario(nome=dados["nome"],idade=dados["idade"])
+   doc = usuario.to_dict()
+   usuarios.insert(doc)
+   return {"mensagem":"Usuario criado com sucesso usando a classe Usuario","usuario":doc},201
+
+u = Usuario("Kaik", 23)
+print(u.__dict__)
+
+
+
+@app.get("/usuarios/ativos")
+def usuarios_ativos():
+    return jsonify(usuarios.search(User.ativo == True))
 # =====================================
 # HOME
 # =====================================
@@ -55,6 +86,15 @@ def criar_usuario():
 @app.get("/usuarios")
 def listar_usuarios():
     return jsonify(usuarios.all()), 200
+
+@app.get("/usuario/email/<email>")
+def buscar_por_email(email):
+    user = usuarios.get(User.email == email)
+
+    if not user:
+        return {"erro": "não encontrado"}, 404
+
+    return jsonify(user)
 
 # =====================================
 # BUSCAR USUÁRIO
